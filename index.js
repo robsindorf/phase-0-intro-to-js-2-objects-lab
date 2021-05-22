8fe139dddd01b9211689c6da3ed54383312c731d
Rob Sindorf <robert.sindorf@gmail.com>
2021-05-21 22:34:13 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Joe',
+    streetAddress: '22 Main'
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newEmployee = { ...obj };
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+const newEmployee = updateEmployeeWithKeyAndValue(employee, 'Sam', '11Broadway');
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee, 'Sam', '12 Broadway');
+
+function deleteFromEmployeeByKey(obj, key) {
+    const employeeClone = { ...obj};
+    delete employeeClone.name;
+    delete employeeClone.streetAddress;
+    return employeeClone;
+}
+
+deleteFromEmployeeByKey(employee.key) 
+
+function destructivelyDeleteFromEmployeeByKey(obj, key, value) {
+    obj[key] = value;
+    delete employee.name;
+    delete employee.streetAddress;
+    return employee;
+}
+destructivelyDeleteFromEmployeeByKey(employee, [employee.name, employee.streetAddress,]);
